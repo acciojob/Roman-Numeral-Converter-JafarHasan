47698c4318eaa6e77c564c5a498c2f9669b33462
JafarHasan <159635643+JafarHasan@users.noreply.github.com>
2024-08-13 22:13:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,31 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
+	
+    let roman = '';
+    const values = Object.values(obj);
 
+    for (let i = 0; i < values.length; i++) {
+        const [symbol, value] = values[i];
+        
+        // Check if subtraction is needed (like IV, IX, etc.)
+        if (i % 2 === 0 && i < values.length - 2) {
+            const nextValue = values[i + 2][1];
+            if (num >= value - nextValue) {
+                roman += values[i + 2][0] + symbol;
+                num -= value - nextValue;
+                continue;
+            }
+        }
+
+        // Add the current symbol to the result as many times as possible
+        while (num >= value) {
+            roman += symbol;
+            num -= value;
+        }
+    }
+
+    return roman;
   //your code here
 
 }
